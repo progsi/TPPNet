2a34fa64ea32a88fb06d20072438b72f05263873
progsi <simon@hachmeier.de>
2023-04-04 10:06:09 +0200

    modified shs-yt-dataset structure

--- gen_shs-yt-1300.py
+++ gen_shs-yt-1300.py
@@ -2,18 +2,22 @@ import pandas as pd
 import argparse
 
 def data_to_ID_list(data):
-    return ['_'.join([str(e) for e in l]) for l in data[["set_id", "candidate_yt_id"]].values.tolist()]
+    return [str(l[0]) + str(int(l[1] > 1)) + '_' + l[2] 
+            for l in data[["set_id", "nlabel", "candidate_yt_id"]].values.tolist()]
 
 def main(path: str):
+    """
+    Transform the SHS-YT-1300 dataset into a textfile as input for TPPNet.
+    Each set_id of the initial sets is mapped to 
+    positives --> setID1_ytID 
+    or negatives --> setID0_ytID 
+    """
     data = pd.read_csv(path, sep=';')
-    data.head()
     
     with open('data/shs-yt-1300.txt', 'w') as f:
         for row in data_to_ID_list(data):
             f.write(row + '\n')
             
-     
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=str, default='data/shs1300.csv')
